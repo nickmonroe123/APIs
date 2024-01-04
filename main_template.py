6c5f882688b0307f39edf2e478b3aaa622a7bff1
m30509 <nm309u@att.com>
2024-01-04 16:17:06 -0600

    Adding in test api

--- main_template.py
+++ main_template.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flasgger import Swagger
 
+import book_review
+
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
 
-class UppercaseText(Resource):
 
+class UppercaseText(Resource):
     def get(self):
         """
         This method responds to the GET request for this endpoint and returns the data in uppercase.
@@ -36,6 +38,52 @@ class UppercaseText(Resource):
 
         return jsonify({"text": text.upper()})
 
+
+class AddRecord(Resource):
+    def post(self):
+        """
+        This method responds to the POST request for adding a new record to the DB table.
+        ---
+        tags:
+        - Records
+        parameters:
+            - in: body
+              name: body
+              required: true
+              schema:
+                id: BookReview
+                required:
+                  - Book
+                  - Rating
+                properties:
+                  Book:
+                    type: string
+                    description: the name of the book
+                  Rating:
+                    type: integer
+                    description: the rating of the book (1-10)
+        responses:
+            200:
+                description: A successful POST request
+            400:
+                description: Bad request, missing 'Book' or 'Rating' in the request body
+        """
+
+        data = request.json
+        print(data)
+
+        if data is not None:
+            success = True
+        else:
+            success = False
+
+        if success:
+            return {"message": "Record added successfully"}, 200
+        else:
+            return {"message": "Failed to add record"}, 500
+
+
+api.add_resource(AddRecord, "/add-record")
 api.add_resource(UppercaseText, "/uppercase")
 
 if __name__ == "__main__":
